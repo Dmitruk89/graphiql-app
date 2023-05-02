97df1509993a9c706d2482a756a3007e046fbd56
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-05-02 15:56:50 +0400

    feat: add editor component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import React from 'react';
 import Footer from '../components/Footer';
+import Editor from '../components/Editor';
 
 function Home() {
   return (
     <>
       <Header></Header>
+      <Editor></Editor>
       <Footer></Footer>
     </>
   );
