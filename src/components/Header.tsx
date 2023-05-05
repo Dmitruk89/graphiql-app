fde3cca79b89dab2d709ea9f7fe43cc3d82558c4
Irina <irenserge@mail.ru>
2023-05-05 17:07:12 +0300

    feat: delete menu-icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,9 +16,6 @@ export default function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {t.header.title}
           </Typography>
