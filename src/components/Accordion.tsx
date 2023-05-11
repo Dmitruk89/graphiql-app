f441d35df7c8209bbf94519b13bbac58888f16ed
Irina <irenserge@mail.ru>
2023-05-11 14:51:57 +0300

    feat: add tabs

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import LabTabs from './Tabs';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
 
 export default function SimpleAccordion() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
   return (
     <>
       <Accordion>
@@ -13,12 +20,20 @@ export default function SimpleAccordion() {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
-        ></AccordionSummary>
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example"
+          >
+            <Tab label="Variables" value="1" />
+            <Tab label="Headers" value="2" />
+          </Tabs>
+        </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+          <LabTabs value={value} />
         </AccordionDetails>
       </Accordion>
     </>
