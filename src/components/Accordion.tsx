b162f0f7cca8d4c4f47c535a8fae3ec6c3e0425f
Irina <irenserge@mail.ru>
2023-05-11 15:50:21 +0300

    fix: fix open/close accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -6,31 +6,51 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import LabTabs from './Tabs';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
+import Box from '@mui/material/Box';
 
 export default function SimpleAccordion() {
   const [value, setValue] = React.useState('1');
+  const [isExpanded, setIsExpanded] = React.useState('auto');
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const handleClick = (event: React.SyntheticEvent) => {
+    const target = event.currentTarget.firstChild?.firstChild?.firstChild?.firstChild?.textContent;
+    // if (target) {
+    //   setIsExpanded('none');
+    // } else {
+    //   setIsExpanded('auto');
+    // }
+  };
+
   return (
     <>
       <Accordion>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
+          expandIcon={
+            <ExpandMoreIcon
+              sx={{ transform: 'rotate(180deg)', pointerEvents: 'auto' }}
+              onClick={() => setIsExpanded('auto')}
+            />
+          }
           aria-controls="panel1a-content"
           id="panel1a-header"
+          sx={{ pointerEvents: `${isExpanded}` }}
         >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            textColor="secondary"
-            indicatorColor="secondary"
-            aria-label="secondary tabs example"
-          >
-            <Tab label="Variables" value="1" />
-            <Tab label="Headers" value="2" />
-          </Tabs>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }} onClick={handleClick}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="primary"
+              indicatorColor="primary"
+              aria-label="tabs example"
+            >
+              <Tab className="Variables" label="Variables" value="1" />
+              <Tab className="Headers" label="Headers" value="2" />
+            </Tabs>
+          </Box>
         </AccordionSummary>
         <AccordionDetails>
           <LabTabs value={value} />
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
 interface IProps {
@@ -13,12 +11,6 @@ export default function LabTabs(props: IProps) {
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
       <TabContext value={props.value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          {/* <TabList onChange={handleChange} aria-label="tabs">
-            <Tab label="Variables" value="1" />
-            <Tab label="Headers" value="2" />
-          </TabList> */}
-        </Box>
         <TabPanel value="1">Variables</TabPanel>
         <TabPanel value="2">Headers</TabPanel>
       </TabContext>
